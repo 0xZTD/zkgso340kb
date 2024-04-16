148184d50c8184b16058ba3f0761848af93f01e2
zestx5 <buyankov.ilya@gmail.com>
2024-04-16 13:06:29 +0300

    add: router

---
+++ src/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sepium;
+
+class Router
+{
+    protected $routes = [];
+
+    public function addRoute($route, $controller, $method)
+    {
+        $this->routes[$route] = ['controller' => $controller, 'method' => $method];
+    }
+
+    public function send($uri)
+    {
+        if (array_key_exists($uri, $this->routes)) {
+            $controller = $this->routes[$uri]['controller'];
+            $meth = $this->routes[$uri]['method'];
+
+            $controller = new $controller();
+            $controller->$meth();
+        } else {
+            throw new \Exception("No route for URI: $uri");
+        }
+    }
+}
